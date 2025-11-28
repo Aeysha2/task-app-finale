390a96b6cc18c5ee9ff66f6e11d52d35c27dd9cf
Aeysha2 <ndaicha27@gmail.com>
2025-11-28 14:42:34 +0000

    add component login

--- task-app/task-app-client/app/authentification/login/login.tsx
+++ task-app/task-app-client/app/authentification/login/login.tsx
@@ -1,3 +1,20 @@
+import { Button } from "../components/button"
+import { Input } from "../components/input"
+
 export const Login = ()=> {
-    return(<h1>Login</h1>)
+    return(
+    <div className="min-h-screen flex items-center justify-center bg-stone-400">
+         <form  className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
+            <h2 className="text-2xl font-semibold mb-6 text-center">Create Account</h2>
+            <div className="space-y-4" >
+    
+               
+               <Input label="Email" type="email" />
+               <Input label="Password" type="password" />
+               <Button title="Connect"/>
+    
+            
+          </div>
+         </form>
+        </div>)
 }
