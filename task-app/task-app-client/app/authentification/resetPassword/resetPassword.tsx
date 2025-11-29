d1f7d2ca5950a00baa980b1cea09086149ea91eb
Aeysha2 <ndaicha27@gmail.com>
2025-11-29 12:46:14 +0000

    add reset password screen

--- task-app/task-app-client/app/authentification/resetPassword/resetPassword.tsx
+++ task-app/task-app-client/app/authentification/resetPassword/resetPassword.tsx
@@ -1,3 +1,16 @@
+import { Button } from "../components/button"
+import { Form } from "../components/form"
+import { FormTitle } from "../components/formTitle"
+import { Input } from "../components/input"
+
 export const ResetPassword = ()=> {
-    return(<h1>ResetPassword</h1>)
+    return(
+    <Form>
+             <div className="space-y-4" >
+                <FormTitle title="Reinitialisation de mot de passe" />
+                <Input label="Password" type="password" />
+                <Input label="Confirm Password" type="password" />
+                <Button title="Reinitialiser" />
+             </div>
+          </Form>)
 }
