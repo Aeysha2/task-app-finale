2bab23173dc96c0cc8ec352736d60e2084cf55d1
Aeysha2 <ndaicha27@gmail.com>
2025-11-29 12:44:50 +0000

    add forgot password screen

--- task-app/task-app-client/app/authentification/forgotPassword/forgotPassword.tsx
+++ task-app/task-app-client/app/authentification/forgotPassword/forgotPassword.tsx
@@ -1,3 +1,15 @@
-export const ForgotPassword = ()=> {
-    return(<h1>ForgotPassword</h1>)
+import { Button } from "../components/button"
+import { Form } from "../components/form"
+import { FormTitle } from "../components/formTitle"
+import { Input } from "../components/input"
+
+export const ForgotPassword = () => {
+    return (
+        <Form>
+            <div className="space-y-4" >
+                <FormTitle title="Mot de passe oublié" />
+                <Input label="Email" type="email" />
+                <Button title="Verification" />
+            </div>
+        </Form>)
 }
