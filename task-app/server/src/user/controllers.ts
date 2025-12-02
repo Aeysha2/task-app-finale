4c5ee7de33fbf3e2eb688030bb5b8f415f25e0f1
Aeysha2 <ndaicha27@gmail.com>
2025-12-02 12:40:32 +0000

    add update user feature

--- task-app/server/src/user/controllers.ts
+++ task-app/server/src/user/controllers.ts
@@ -7,33 +7,36 @@ const prisma = new PrismaClient()
 UserRouter
     .get("/", async (request, response) => {
         const users = await prisma.user.findMany()
-
         response.json({ users })
     })
 
     .get("/:id", async (request, response) => {
-
         const user = await prisma.user.findUnique({ where: { id: request.params.id } })
-
         response.json({ user })
     })
 
+
+
+    .put("/:id", async (request, response) => {
+        const userID = request.params.id
+        const user = await prisma.user.findUnique({ where: { id: request.params.id } })
+        if (!user) throw new Error(`Utilisateur non trouver avec cet id. ${userID}`)
+        const { Firstname,Lastname,Email } = request.body
+
+            
+        const userUpdated = await prisma.user.update({
+                        where: { id: userID }, 
+                        data: { Firstname,Lastname,Email }
+                    })
+        response.json({ user })
+    })
+    
+
     .post("/", async (request, response) => {
-        const {
-            Firstname,
-            Lastname,
-            Email,
-            Password
-        } = request.body
+        const { Firstname,Lastname,Email,Password } = request.body
         prismaClient.user.create({ data: { Email, Firstname, Lastname, Password } })
-        await prismaClient.user.create({ data: { Email, Firstname, Lastname, Password } })
+        const user = await prismaClient.user.create({ data: { Email, Firstname, Lastname, Password } })
+        response.json({ user })
 
-        response.send(` utilisateur:
         
-         ${request.body.Firstname}
-         ${request.body.Lastname}
-         ${request.body.Email}
-         ${request.body.Password}
-
-         `)
     }) 
