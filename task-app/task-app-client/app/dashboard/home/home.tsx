947dac5fc0f58ead2c51675aed2d6de43471927e
Aeysha2 <ndaicha27@gmail.com>
2025-12-09 00:00:43 +0000

    handler userlogged

--- task-app/task-app-client/app/dashboard/home/home.tsx
+++ task-app/task-app-client/app/dashboard/home/home.tsx
@@ -6,24 +6,25 @@ import type { UserLogged } from "~/types"
 import { getUserFromStorage } from "~/utils/getUserLogged"
 
 export const Home = () => {
-    const [user, setUser] = useState < string|null>(null)
+    const [user, setUser] = useState < UserLogged|null>(null)
     const [tasksVisibility,setTasksVisibility] = useState(true)
 
     const toggle = () => {
         setTasksVisibility(!tasksVisibility)
     }
+
     useEffect(() => {
         const userLogged:UserLogged |null = getUserFromStorage()
-        setUser(localStorage.getItem(userLoggedKey))
-        console.log("yacou",userLogged)
-    },[])
-
+        setUser(userLogged)  
+    },[user])
     return (
         <>
             <div className="bg-blue-600 p-10 gap-40 flex justify-center items-center">
                 <div className="flex items-center gap-2">
-                    <div className="w-10 h-10 bg-white flex  justify-center items-center rounded-full uppercase">AN</div>
-                    <small className="text-base uppercase">Aissatou Ndao</small>
+                    <div className="w-10 h-10 bg-white flex  justify-center items-center rounded-full uppercase">{user?.Firstname[0]}{user?.Lastname[0]}</div>
+                    <small className="text-base uppercase">
+                        {user?.Firstname} {user?.Lastname}
+                    </small>
                 </div>
                 <h1 className="text-white text-3xl">Application de Gestion de Tâches</h1>
             </div>
