4db86148f21401527a40d763d7f6af5f77f80b5b
Aeysha2 <ndaicha27@gmail.com>
2025-12-13 17:54:49 +0000

    Fix: corrected forgotPassword email subject and HTML formatting, and move forgotPassword position

--- task-app/server/src/user/controllers.ts
+++ task-app/server/src/user/controllers.ts
@@ -13,20 +13,21 @@ UserRouter
         response.json({ user: await findAll() })
     })
 
-    .get("/:id", Auth, async (request, response) => {
-        response.json({ user: await findById(request.params.id) })
-    })
-
-    .get("/forgotPassword/:Email", Auth, async (request, response) => {
+    .get("/forgotPassword/:Email", async (request, response) => {
         try {
             await forgotPassword(request.params.Email)
             response.json({
                 message: `Verifier votre email(${request.params.Email}), nous vous avons envoyé un lien de reinitialisation`
             })
         } catch (error: any) {
-            response.status(403).json({ message: "generation de lien de reiitialisation de mot de passe echouer" })
-
+            response.status(403).json({ 
+            message: "generation de lien de reiitialisation de mot de passe echouer" 
+        })
         }
+    })
+
+    .get("/:id", Auth, async (request, response) => {
+        response.json({ user: await findById(request.params.id) })
     })
 
     .put("/:id", Auth, async (request, response) => {
@@ -41,7 +42,10 @@ UserRouter
 
     .post("/login", async (request, response) => {
         try {
-            const user = await loginUser({ Email: request.body.Email, Password: request.body.Password })
+            const user = await loginUser({
+                Email: request.body.Email,
+                Password: request.body.Password
+            })
             const token = generateToken(user.id, user.Email)
             response.json({ token })
         }
--- task-app/server/src/user/service.ts
+++ task-app/server/src/user/service.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { CreateUser, UpdateUser, LoginUser } from "@src/type";
 import { generateToken } from "../utils/jwt.js";
-import { hash ,compare} from "bcrypt";
+import { hash, compare } from "bcrypt";
 import { sendEmail } from "../utils/sendMail.js";
 
 
@@ -11,51 +11,64 @@ export const findAll = async () => {
     return await prisma.user.findMany()
 }
 
-export const findById = async (id:string) => {
-        return await prisma.user.findUnique({ 
-            where: { id }
-         })
-    }
+export const findById = async (id: string) => {
+    return await prisma.user.findUnique({
+        where: { id }
+    })
+}
 
-export const forgotPassword = async (Email:string) => {
-        const user = await prisma.user.findUnique({ 
-            where: { Email }
-         })
-        if (!user) throw new Error(`Utilisateur non trouver avec cet Email. ${Email}`)
-        const token = generateToken(user.id, user.Email)
-        const link = `http://localhost:5173/resetPassword/${token}`
-        await sendEmail({to:Email, subject:link, html:"Reinitialisation de mot de passe"})
+export const forgotPassword = async (Email: string) => {
+    const user = await prisma.user.findUnique({
+        where: { Email }
+    })
+    if (!user) throw new Error(`Utilisateur non trouver avec cet Email. ${Email}`)
+    const token = generateToken(user.id, user.Email)
+    // const link = `http://localhost:5173/resetPassword/${token}`
+    await sendEmail({
+        to: Email,
+        subject: "Réinitialisation de mot de passe",
+        html: `
+        <p>Bonjour,</p>
+        <p>Veuillez cliquer sur le lien ci-dessous pour réinitialiser votre mot de passe :</p>
+        <p>
+            <a href="http://localhost:5173/resetPassword/${token}">
+                Réinitialiser mon mot de passe
+            </a>
+        </p>
+        <p>Si vous n'êtes pas à l'origine de cette demande, ignorez simplement cet email.</p>
+    `})
 
-    }
+}
 
-export const updateById = async (id:string, body:UpdateUser) => {
-        const user = await prisma.user.findUnique({ where: {id} })
+export const updateById = async (id: string, body: UpdateUser) => {
+    const user = await prisma.user.findUnique({ where: { id } })
     if (!user) throw new Error(`Utilisateur non trouver avec cet id. ${id}`)
-    if (!body.Firstname )  body.Firstname = user.Firstname 
-    if (!body.Lastname )  body.Lastname = user.Lastname 
-    if (!body.Email )  body.Email = user.Email 
+    if (!body.Firstname) body.Firstname = user.Firstname
+    if (!body.Lastname) body.Lastname = user.Lastname
+    if (!body.Email) body.Email = user.Email
 
-        return await prisma.user.update({
-                        where: { id }, 
-                        data: body
-                    })
-    }   
-    
+    return await prisma.user.update({
+        where: { id },
+        data: body
+    })
+}
+
 export const createUser = async (body: CreateUser) => {
     body.Password = await hash(body.Password, 10)
-    return await prisma.user.create({ 
-        data: body ,
+    return await prisma.user.create({
+        data: body,
     })
-    
+
 }
 
 export const loginUser = async (body: LoginUser) => {
-   const user = await prisma.user.findUnique({
-     where: {Email:body.Email}})
-     if(!user) throw new Error ("l‘utiliateur n‘existe pas")
-     const isSamePassword = await compare(body.Password,user.Password)
-     const {Password, ...userWithoutPassword} = user
-     if(isSamePassword) return userWithoutPassword
-     throw new Error(" Mots de passe incorrect")
+    const user = await prisma.user.findUnique({
+        where: { Email: body.Email }
+    })
+    if (!user) throw new Error("l‘utiliateur n‘existe pas")
+    const isSamePassword = await compare(body.Password, user.Password)
+    const { Password, ...userWithoutPassword } = user
+    if (isSamePassword) return userWithoutPassword
+    throw new Error(" Mots de passe incorrect")
 
 }
