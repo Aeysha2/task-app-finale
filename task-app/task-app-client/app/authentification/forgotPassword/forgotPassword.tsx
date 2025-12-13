8e63ff8d634a78bae3f081409ce0d0ac56c71731
Aeysha2 <ndaicha27@gmail.com>
2025-12-13 17:47:08 +0000

    handler forgotPassword

--- task-app/task-app-client/app/authentification/forgotPassword/forgotPassword.tsx
+++ task-app/task-app-client/app/authentification/forgotPassword/forgotPassword.tsx
@@ -1,20 +1,27 @@
+import { baseUrl } from "~/utils/constante"
 import { AuthInfo } from "../components/authInfo"
 import { Button } from "../components/button"
 import { Form } from "../components/form"
 import { FormTitle } from "../components/formTitle"
 import { Input } from "../components/input"
+import { useState } from "react"
 
 export const ForgotPassword = () => {
+    const [Email,setEmail] = useState("")
     const handlerForgotPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault()
-        console.log("handlerForgotPassword")
-    }
+              fetch(`${baseUrl}/users/forgotPassword/${Email}`)
+                  .then(response => response.json())
+                  .then((data) => {})
+                  .catch((error) => {
+                  console.error("Echec  de la connexion:", error)})
+      };
     return (
         <Form>
             <div className="space-y-4" >
                 <FormTitle title="Mot de passe oublié" />
-                <Input label="Email" type="email" />
-                <Button title="Verification" onclick={handlerForgotPassword} />
+                <Input label="Email" type="email" onChange={setEmail}/>
+                <Button title="Verification de l’email" onclick={handlerForgotPassword} />
                 <AuthInfo action="connectez-vous" answer="vous vous souvenez de votre mot de passe  " url="/" />
             </div>
         </Form>)
--- task-app/task-app-client/app/routes.ts
+++ task-app/task-app-client/app/routes.ts
@@ -1,11 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-
     index("routes/login.tsx"),
     route("register","routes/register.tsx"),
     route("forgotPassword","routes/forgotPassword.tsx"),
-    route("resetPassword","routes/resetPassword.tsx"),
+    route("resetPassword/:token","routes/resetPassword.tsx"),
     route("home","routes/home.tsx"),
     route("dashboard","routes/dashboard.tsx"),
 ] satisfies RouteConfig;
