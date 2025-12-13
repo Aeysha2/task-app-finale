8b56843cf2665f9a261d3fe40dce5943540fc143
Aeysha2 <ndaicha27@gmail.com>
2025-12-13 15:40:53 +0000

    handler task PENDING and STARTING

--- task-app/task-app-client/app/dashboard/components/taskItem.tsx
+++ task-app/task-app-client/app/dashboard/components/taskItem.tsx
@@ -3,8 +3,6 @@ import { baseUrl } from "~/utils/constante";
 import { getTokenFromStorage } from "~/utils/getUserLogged";
 
 export const  TaskItem = ({task} : {task?:TaskParams}) => {
-
-
   const handleStart = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
           fetch(`${baseUrl}/tasks/starting/${task?.id}`, {
@@ -18,7 +16,23 @@ export const  TaskItem = ({task} : {task?:TaskParams}) => {
               .then(({task}) => {
               })
               .catch((error) => {
-                          console.error("Echec  de la connexion:", error)})
+              console.error("Echec  de la connexion:", error)})
+  };
+
+    const handleFinishing = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+          fetch(`${baseUrl}/tasks/finishing/${task?.id}`, {
+                method: "PATCH",
+                headers: {
+                  "Content-type": "application/json" ,         
+                  "Authorization": `Bearer ${getTokenFromStorage()}`
+                },
+              })
+              .then(response => response.json())
+              .then(({task}) => {
+              })
+              .catch((error) => {
+              console.error("Echec  de la connexion:", error)})
   };
 
   const handleDelete = () => {
@@ -28,7 +42,18 @@ export const  TaskItem = ({task} : {task?:TaskParams}) => {
   const handleEdit = () => {
     alert("Mode édition activé !");
   };
-
+const startingButton = <button
+          onClick={handleStart}
+          className="px-3 py-1 rounded-xl bg-blue-500 text-white hover:bg-blue-600 transition"
+        >
+          Commencer
+        </button>
+const finishingButton = <button
+          onClick={handleFinishing}
+          className="px-3 py-1 rounded-xl bg-green-500 text-white hover:bg-green-600 transition"
+        >
+          Terminer
+        </button>
   return (
     <div className="max-w-md bg-white shadow-md rounded-2xl p-4 border">
       <h3 className="text-lg font-bold mb-1">{task?.title}</h3>
@@ -54,12 +79,10 @@ export const  TaskItem = ({task} : {task?:TaskParams}) => {
         >
           Supprimer
         </button>
-        <button
-          onClick={handleStart}
-          className="px-3 py-1 rounded-xl bg-blue-500 text-white hover:bg-blue-600 transition"
-        >
-          Commencer
-        </button>
+        { task?.Status === "PENDING"
+        ? startingButton
+        : task?.Status === "STARTING" ? finishingButton : "Tâche bien terminée vous pouvez la modifier et recommencer"
+        }
       </div>
     </div>
   );
