b8e5508fb5f64b7aa8f18eec1d004f58c1e66b6a
Aeysha2 <ndaicha27@gmail.com>
2025-12-14 18:28:18 +0000

    fix main.ts and package.json for render production

--- task-app/server/src/main.ts
+++ task-app/server/src/main.ts
@@ -1,17 +1,33 @@
-import EXPRESS from "express";
+import EXPRESS, { response } from "express";
 import { UserRouter } from "./user/controllers.js";
 import { TaskRouter } from "./task/controllers.js";
 import cors from "cors"
 import { Auth } from "./middleware/auth.js";
+import path from "path";
+import dotenv from "dotenv";
+import { request } from "http";
+dotenv.config();
 
 
 const application = EXPRESS ()
-const port = 3000
+const port = process.env.PORT || 3000
 application.use ( cors ( ) )
 application.use(EXPRESS.json())
 application.use(EXPRESS.urlencoded({}))
 application.use("/users",UserRouter)
 application.use("/tasks",Auth,TaskRouter)
+
+if (process.env.NODE_ENV === "production") {
+    application.use(
+        EXPRESS.static(
+            path.join(__dirname, "../../task-app-client/build")
+        )
+    )
+    application.get("*", (request, response)=>{
+        response.sendFile(
+            path.join(__dirname, "../../task-app-client/build/index.html")
+        )
+    })}
 
 
 application.listen(port,()=> console.log(`connecter sur http://localhost:${port}`))
