657140e000806199580043fa9f8dff6b4717c023
Aeysha2 <ndaicha27@gmail.com>
2025-12-14 20:49:25 +0000

    update constante.ts for render

--- task-app/task-app-client/app/utils/constante.ts
+++ task-app/task-app-client/app/utils/constante.ts
@@ -1,2 +1,4 @@
-export const baseUrl = "http://localhost:3000"
+export const baseUrl = import.meta.env.PROD
+    ? "https://task-app-backend-fzxb.onrender.com"
+    : "http://localhost:3000";
 export const userLoggedKey = "userLoggedKey"
